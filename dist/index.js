a2a2f2b5efbc3094e0c746e5d1fdc926eccccb16
yzfdldx <yzf@126.com>
2019-08-17 15:22:23 +0800

    web

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-  // const file = require('../dist/index.html');
-  // res.sendfile(file)
-  // res.send('32')
-  // res.render('app');
+  res.render('index', { title: 'ushance.com' });
 });
 
 module.exports = router;
