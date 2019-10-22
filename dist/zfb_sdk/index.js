0cf6d5ab9cf39a93a33af048ee2564b52ed16d5e
yzfdldx <yzf@126.com>
2019-10-22 20:16:02 +0800

    ok

--- dist/zfb_sdk/index.js
+++ dist/zfb_sdk/index.js
@@ -4,9 +4,8 @@ var AlipaySdk = require('alipay-sdk').default;
 var AlipayFormData = require('alipay-sdk/lib/form').default;
 var https = require('https');
 var fs = require('fs');
-var crypto = require('crypto');
 
-var alipaySdk = new AlipaySdk({
+const alipaySdk = new AlipaySdk({
   // 参考下方 SDK 配置
   // url: 'https://openapi.alipay.com/gateway.do',
   appId: '2019101268311851',
@@ -15,11 +14,27 @@ var alipaySdk = new AlipaySdk({
   privateKey: fs.readFileSync('./public/ssl/zfb/private-key.pem', 'ascii'), // 应用私钥字符串
   alipayPublicKey: fs.readFileSync('./public/ssl/zfb/public-key.pem', 'ascii'), // 支付宝公钥
 });
-// console.log(21)
 
-var checkFn = (e, query, res) => {
+
+function download ( url, callback ) {
+  https.get( url,function(res){
+    var data = "";
+    res.setEncoding('utf8');
+    res.on("data",function(chunk){
+      data += chunk;
+    });
+    res.on("end",function(){
+      callback(data)
+    })
+  }).on("error",function(err){
+    console.log(err)
+    callback()
+  })
+}
+
+const checkFn = (e, query, res) => {
   if (query && e) {
-    var onoff = true;
+    let onoff = true;
     e.forEach(i => {
       if (!query[i]) {
         res.send({
@@ -35,53 +50,15 @@ var checkFn = (e, query, res) => {
   return false
 }
 
-//将支付宝发来的数据生成有序数列
-function getVerifyParams(params) {
-  var sPara = [];
-  if(!params) return null;
-  for(var key in params) {
-      if((!params[key]) || key == "sign" || key == "sign_type") {
-          continue;
-      };
-      sPara.push([key, params[key]]);
-  }
-  sPara = sPara.sort();
-  var prestr = '';
-  for(var i2 = 0; i2 < sPara.length; i2++) {
-      var obj = sPara[i2];
-      if(i2 == sPara.length - 1) {
-          prestr = prestr + obj[0] + '=' + obj[1] + '';
-      } else {
-          prestr = prestr + obj[0] + '=' + obj[1] + '&';
-      }
-  }
-  return prestr;
-}
-
-function veriySign(params) {
-  try {
-      var publicPem = fs.readFileSync('./public/ssl/zfb/private-public-key.pem'); // 应用公钥字符串
-      var publicKey = publicPem.toString();
-      var prestr = getVerifyParams(params);
-      var sign = params['sign'] ? params['sign'] : "";
-      var verify = crypto.createVerify('RSA-SHA1');
-      verify.update(prestr);
-      return verify.verify(publicKey, sign, 'base64')
-  } catch(err) {
-      console.log('veriSign err', err)
-  }
-}
-
 var partner = '';
 
 /* GET */
+// 支付连接
 router.get('/web/pay.json', async function(req, res, next) {
-  partner = '';
   try {
-    var query = req.query;
+    const query = req.query;
     if (checkFn(['id', 'price', 'name', 'describe'], query, res)) {
-      var formData = new AlipayFormData();
-      partner = query.id,
+      const formData = new AlipayFormData();
       formData.setMethod('get');
       formData.addField('notifyUrl', 'https://www.ushance.com/zfb_sdk/web/yanqian.json');
       formData.addField('bizContent', {
@@ -91,7 +68,7 @@ router.get('/web/pay.json', async function(req, res, next) {
         subject: query.name,
         body: query.describe,
       });
-      var result = await alipaySdk.exec(
+      const result = await alipaySdk.exec(
         'alipay.trade.page.pay',
         {},
         { formData: formData },
@@ -111,18 +88,55 @@ router.get('/web/pay.json', async function(req, res, next) {
   }
 });
 
-router.post('/web/yanqian2.json', async function(req, res, next) {
+router.get('/web/pay2.json', async function(req, res, next) {
+  partner = '';
+  try {
+    const query = req.query;
+    if (checkFn(['id', 'price', 'name', 'describe'], query, res)) {
+      const formData = new AlipayFormData();
+      partner = query.id,
+      formData.setMethod('get');
+      formData.addField('notifyUrl', 'https://www.ushance.com/zfb_sdk/web/yanqian.json');
+      formData.addField('bizContent', {
+        outTradeNo: query.id, // 唯一值
+        productCode: 'FAST_INSTANT_TRADE_PAY',
+        totalAmount: query.price,
+        subject: query.name,
+        body: query.describe,
+      });
+      const result = await alipaySdk.exec(
+        'alipay.trade.page.pay',
+        {},
+        { formData: formData },
+      );
+      res.send({
+        result: result,
+        errorCode: 200,
+        message: '',
+      });
+    }
+  } catch (error) {
+    res.send({
+      result: null,
+      errorCode: 'err',
+      message: '代码出错了',
+    });
+  }
+});
+
+// 验证
+router.post('/web/yanqian.json', async function(req, res, next) {
   // res.send({
   //   result: req,
   //   errorCode: 200,
   //   message: 'ok',
   // });
-  console.log('yanqian.json', req)
-  var postData = {
-    appId: '2019101268311851',
-    outTradeNo: '20150320010101001',
-  }
-  var verify = await alipaySdk.checkNotifySign(postData);
+  // console.log('yanqian.json', req)
+  // const postData = {
+  //   appId: '2019101268311851',
+  //   outTradeNo: '20150320010101001',
+  // }
+  const verify = await alipaySdk.checkNotifySign(req.body);
   res.send({
     result: verify,
     errorCode: 'err',
@@ -130,7 +144,7 @@ router.post('/web/yanqian2.json', async function(req, res, next) {
   });
 });
 
-router.get('/web/yanqian.json', async function(req, res, next) {
+router.get('/web/yanqian2.json', async function(req, res, next) {
   console.log(req.body)
   var params = req.body
   var mysign = veriySign(params);
@@ -162,7 +176,7 @@ router.get('/web/yanqian.json', async function(req, res, next) {
 });
 
 router.get('/web/okPay.json', async function(req, res, next) {
-  var postData = {
+  const postData = {
     appId: '2019101268311851',
     charset: 'utf-8',
     version: '1.0',
@@ -179,7 +193,7 @@ router.get('/web/okPay.json', async function(req, res, next) {
       body: '商品详情'
     }
   }
-  var verify = await alipaySdk.checkNotifySign(postData);
+  const verify = await alipaySdk.checkNotifySign(postData);
   res.send({
     result: verify,
     errorCode: 'err',
@@ -187,53 +201,147 @@ router.get('/web/okPay.json', async function(req, res, next) {
   });
 });
 
+// 查询
 router.get('/web/query.json', async function(req, res, next) {
-  var result = await alipaySdk.exec(
-    'alipay.trade.query',
-    {},
-    { formData: formData },
-  );
-  res.send({
-    result: verify,
-    errorCode: 'err',
-    message: '1221',
-  });
+  try {
+    const query = req.query;
+    if (checkFn(['id', 'price', 'name', 'describe'], query, res)) {
+      const formData = new AlipayFormData();
+      formData.setMethod('get');
+      formData.addField('notifyUrl', 'https://www.ushance.com/zfb_sdk/web/yanqian.json');
+      formData.addField('bizContent', {
+        outTradeNo: query.id, // 唯一值
+        productCode: 'FAST_INSTANT_TRADE_PAY',
+        totalAmount: query.price,
+        subject: query.name,
+        body: query.describe,
+      });
+      const result = await alipaySdk.exec(
+        'alipay.trade.query',
+        {},
+        { formData: formData },
+      );
+      if (result) {
+        download(result, function( data ) {
+          if(data){
+            res.send({
+              data: data,
+              result: 'succeed',
+              errorCode: 200,
+              message: '',
+            });
+          } else {
+            res.send({
+              result: 'error',
+              errorCode: 200,
+              message: '查询失败',
+            });
+          }
+        })
+      } else {
+        res.send({
+          result: 'error',
+          errorCode: 200,
+          message: '查询失败',
+        });
+      }
+    }
+  } catch (error) {
+    res.send({
+      result: null,
+      errorCode: 'err',
+      message: '代码出错了',
+    });
+  }
 });
 
 // 退款
 router.get('/web/refund.json', async function(req, res, next) {
-  var formData = new AlipayFormData();
-  formData.addField('notifyUrl', 'https://www.ushance.com/');
-  formData.addField('bizContent', {
-    outTradeNo: '20150320010101001', // 唯一值
-    productCode: 'FAST_INSTANT_TRADE_PAY',
-    totalAmount: '0.01',
-    subject: '商品',
-    body: '商品详情',
-  });
-  var result = await alipaySdk.exec(
-    'alipay.trade.query',
-    {},
-    { formData: formData },
-  );
-  res.send({
-    result: verify,
-    errorCode: 'err',
-    message: '1221',
-  });
+  if (checkFn(['id', 'price', 'name', 'describe'], query, res)) {
+    const formData = new AlipayFormData();
+    formData.setMethod('get');
+    formData.addField('notifyUrl', 'https://www.ushance.com/zfb_sdk/web/yanqian.json');
+    formData.addField('bizContent', {
+      outTradeNo: query.id, // 唯一值
+      productCode: 'FAST_INSTANT_TRADE_PAY',
+      totalAmount: query.price,
+      subject: query.name,
+      body: query.describe,
+    });
+    const result = await alipaySdk.exec(
+      'alipay.trade.refund',
+      {},
+      { formData: formData },
+    );
+    if (result) {
+      download(result, function( data ) {
+        if(data){
+          res.send({
+            data: data,
+            result: 'succeed',
+            errorCode: 200,
+            message: '',
+          });
+        } else {
+          res.send({
+            result: 'error',
+            errorCode: 200,
+            message: '退款失败',
+          });
+        }
+      })
+    } else {
+      res.send({
+        result: 'error',
+        errorCode: 200,
+        message: '退款失败',
+      });
+    }
+  }
 });
 
 router.get('/web/refundQuery.json', async function(req, res, next) {
-  var result = await alipaySdk.exec(
-    'alipay.trade.query',
-    {},
-    { formData: formData },
-  );
-  res.send({
-    result: verify,
-    errorCode: 'err',
-    message: '1221',
-  });
+  if (checkFn(['id', 'price', 'name', 'describe'], query, res)) {
+    const formData = new AlipayFormData();
+    formData.setMethod('get');
+    formData.addField('notifyUrl', 'https://www.ushance.com/zfb_sdk/web/yanqian.json');
+    formData.addField('bizContent', {
+      outTradeNo: query.id, // 唯一值
+      productCode: 'FAST_INSTANT_TRADE_PAY',
+      refund_amount: query.price,
+      subject: query.name,
+      body: query.describe,
+    });
+    const result = await alipaySdk.exec(
+      'alipay.trade.fastpay.refund.query',
+      {},
+      { formData: formData },
+    );
+    if (result) {
+      download(result, function( data ) {
+        if(data){
+          res.send({
+            data: data,
+            result: 'succeed',
+            errorCode: 200,
+            message: '',
+          });
+        } else {
+          res.send({
+            result: 'error',
+            errorCode: 200,
+            message: '查询失败',
+          });
+        }
+      })
+    } else {
+      res.send({
+        result: 'error',
+        errorCode: 200,
+        message: '查询失败',
+      });
+    }
+  }
 });
 
 module.exports = router;
