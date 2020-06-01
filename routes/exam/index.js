a2d7de7a862b0305594da6d636641cdff28a2d28
yzfdldx <yzf@126.com>
2020-06-01 21:27:57 +0800

    ok

--- routes/exam/index.js
+++ routes/exam/index.js
@@ -958,6 +958,124 @@ router.post('/edit_random.json', function(req, res, next) { // 答题编辑
     });
   }
 });
+router.post('/re_random.json', function(req, res, next) { // 重新答题
+  try {
+    // const query = req.query;
+    const query = req.body;
+    if (checkFn(['test_id'], query, res)) {
+      var select_test = 'select ' + '*' + ' from ' + 'my_web.exam_test' + ' where ' + `id = "${query.test_id}"`;
+      MQ_ok(select_test, res, (result_test) => {
+        // const result_a = {insertId: 6}
+        if (result_test) {
+          var select = 'select ' + '*' + ' from ' + 'my_web.exam_lists';
+          MQ_ok(select, res, (result) => {
+            if (result) {
+              const list = new Array(parseInt(result_test[0].len)).fill('a').map(e => {
+                const a = Rand(0, result.length - 1);
+                return result.splice(a, 1)[0].id;
+              })
+              let str = `start_time = '${DFormat()}'`;
+              str += `, end_time = null`;
+              str += `, time_len = null`;
+              str += `, test_list = null`;
+              str += `, get_mark = null`;
+              str += `, test_list = '${JSON.stringify(list)}'`;
+
+              var select3 = `update my_web.exam_random set ` +
+              str +
+              ` where id = ${query.id}`;
+              MQ_ok(select3, res, (result3) => { // 更新随机试卷
+                if (result3 && result3[0]) {
+                  res.send({
+                    result: 'succeed',
+                    data: result3[0],
+                  });
+                } else {
+                  res.send({
+                    result: 'succeed',
+                    data: {},
+                  });
+                }
+              })
+            }
+          })
+        } else {
+          res.send({
+            result: 'error',
+            data: {},
+          });
+        }
+      })
+    }
+    if (checkFn(['id', 'test_list', 'start_time', 'end_time', 'time_len'], query, res)) {
+      var select = 'select ' + '*' + ' from ' + 'my_web.exam_random' + ' where ' + `id = "${query.id}"`
+      MQ_ok(select, res, (result) => { // 查看随机试卷
+        if (result && result[0]) {
+          let Arr = [];
+          let Asn = [];
+          try {
+            Arr = JSON.parse(result[0].lists);
+            Asn = JSON.parse(query.test_list);
+          } catch (error) {
+            //
+          }
+          let Str = '';
+          Arr.forEach(e => {
+            if (!Str) {
+              Str = `id = "${e}"`;
+            } else {
+              Str += `or id = "${e}"`;
+            }
+          })
+          var select2 = 'select ' + '*' + ' from ' + 'my_web.exam_lists' + ' where ' + Str
+          MQ_ok(select2, res, (result2) => { // 查询所有随机到的题目
+            if (result2) {
+              let mark = 0;
+              result2.forEach((e, k) => {
+                if (e.solution === Asn[k]) {
+                  mark += parseFloat(result[0].mark);
+                }
+              })
+              let str = `start_time = '${query.start_time}'`;
+              str += `, end_time = '${query.end_time}'`;
+              str += `, time_len = '${query.time_len}'`;
+              str += `, test_list = '${query.test_list}'`;
+              str += `, get_mark = '${mark}'`;
+
+              var select3 = `update my_web.exam_random set ` +
+              str +
+              ` where id = ${query.id}`;
+              MQ_ok(select3, res, (result3) => { // 更新随机试卷
+                if (result3 && result3[0]) {
+                  res.send({
+                    result: 'succeed',
+                    data: result3[0],
+                  });
+                } else {
+                  res.send({
+                    result: 'succeed',
+                    data: {},
+                  });
+                }
+              })
+            }
+          })
+        } else {
+          res.send({
+            result: 'error',
+            message: '不存在该用户',
+          });
+        }
+      })
+    }
+  } catch (error) {
+    res.send({
+      result: 'error',
+      errorCode: error,
+      message: '未知错误',
+    });
+  }
+});
 /* 获取学习 */
 router.get('/get_learn_list.json', function(req, res, next) { // 查询学习列表
   try {
