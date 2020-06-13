aafac373c323e30b321f682fbd615f7b5f1f500e
yzfdldx <yzf@126.com>
2020-06-13 17:02:59 +0800

    ok

--- app.js
+++ app.js
@@ -66,7 +66,12 @@ var hostType = 'www';
 app.use((req, res, next)=>{
   try {
     var host = req.host.split('.')[0];
-    console.log('your-host', req, req.hos, host)
+    console.log("headers = " + JSON.stringify(req.headers));// 包含了各种header，包括x-forwarded-for(如果被代理过的话)
+    console.log("x-forwarded-for = " + req.header('x-forwarded-for'));// 各阶段ip的CSV, 最左侧的是原始ip
+    console.log("ips = " + JSON.stringify(req.ips));// 相当于(req.header('x-forwarded-for') || '').split(',')
+    console.log("remoteAddress = " + req.connection.remoteAddress);// 未发生代理时，请求的ip
+    console.log("ip = " + req.ip);// 同req.connection.remoteAddress, 但是格式要好一些
+    console.log('your-host', req.connection, req.parames)
     hostType = host ? host : 'www';
     if (hostType === 'www') {
       index(req, res, next)
