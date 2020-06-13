0a5cac09733d251a78902bf6631e8e9f6ebe70c3
yzfdldx <yzf@126.com>
2020-06-13 18:36:56 +0800

    ok

--- app.js
+++ app.js
@@ -58,9 +58,9 @@ app.all('*', function(req, res, next) {
     //   res.header("Access-Control-Allow-Origin", "*");
     // }
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    // res.header("Access-Control-Allow-Headers", "*");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 // app.all('*', function(req, res, next) {
